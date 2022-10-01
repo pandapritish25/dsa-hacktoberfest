a376369f20b1f0bd5b912a641a3a2e78c4aaaaa7
Mohit Patni <mohitpatni0731@gmail.com>
2022-10-01 21:22:30 +0530

    Create stack.py

---
+++ Stacks/stack.py
@@ -0,0 +1,11 @@
+my_stack = []   
+my_stack.append('x')  
+my_stack.append('y')  
+my_stack.append('z')    
+print(my_stack)  
+print('\nElements poped from my_stack:')  
+print(my_stack.pop())  
+print(my_stack.pop())  
+print(my_stack.pop())  
+print('\nmy_stack after elements are poped:')  
+print(my_stack)   
